5894a6906f090324271274df36c043a67cebef89
krisine <kristine.nyaga@student.moringaschool.com>
2025-07-21 17:12:50 +0300

    updating usage logs

--- src/app/components/Dashboard/inventory/usage/UsageLogs.tsx
+++ src/app/components/Dashboard/inventory/usage/UsageLogs.tsx
@@ -115,7 +115,7 @@ export default function UsageLogs() {
           Resource Usage Logs
         </h1>
 
-        {/* ✅ KPI Cards */}
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8">
           <KPICard
             title="Total Resources Used Today"
